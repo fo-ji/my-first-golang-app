3f39dec06722de7685da2ad59f1b875957c03e34
fo-ji <fumiya1753@gmail.com>
2025-01-19 15:11:16 +0900

    feat: ログの設定

--- config/config.go
+++ config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"log"
+	"my-first-golang-app/utils"
 
 	"gopkg.in/ini.v1"
 )
@@ -17,6 +18,7 @@ var Config ConfigList
 
 func init() {
 	LoadConfig()
+	utils.LoggingSettings(Config.LogFile)
 }
 
 func LoadConfig() {
