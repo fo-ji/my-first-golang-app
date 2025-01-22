06a4391be68de64cf0e6808c33b81364856d4e8e
fo-ji <fumiya1753@gmail.com>
2025-01-22 15:35:44 +0900

    feat: create user

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.23.4
 
 require (
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/mattn/go-sqlite3 v1.14.24 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,15 +2,24 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"my-first-golang-app/config"
+	"my-first-golang-app/app/models"
 )
 
 func main() {
-	fmt.Println(config.Config.Port)
-	fmt.Println(config.Config.SQLDriver)
-	fmt.Println(config.Config.DbName)
-	fmt.Println(config.Config.LogFile)
+	// fmt.Println(config.Config.Port)
+	// fmt.Println(config.Config.SQLDriver)
+	// fmt.Println(config.Config.DbName)
+	// fmt.Println(config.Config.LogFile)
 
-	log.Println("test")
+	// log.Println("test")
+
+	fmt.Println(models.Db)
+
+	u := &models.User{}
+	u.Name = "test"
+	u.Email = "test@example.com"
+	u.Password = "testtest"
+	fmt.Println(u)
+
+	u.CreateUser()
 }
