4ba48349df2dae7f45c30f4a84dd051851f29dc0
fo-ji <fumiya1753@gmail.com>
2025-01-25 14:05:26 +0900

    feat: setup server

--- main.go
+++ main.go
@@ -1,40 +1,7 @@
 package main
 
-import (
-	"my-first-golang-app/app/models"
-)
+import "my-first-golang-app/app/controllers"
 
 func main() {
-	// fmt.Println(config.Config.Port)
-	// fmt.Println(config.Config.SQLDriver)
-	// fmt.Println(config.Config.DbName)
-	// fmt.Println(config.Config.LogFile)
-
-	// log.Println("test")
-
-	// fmt.Println(models.Db)
-
-	// u := &models.User{}
-	// u.Name = "test"
-	// u.Email = "test@example.com"
-	// u.Password = "testtest"
-	// fmt.Println(u)
-
-	// u.CreateUser()
-
-	// u, _ := models.GetUser(1)
-
-	// fmt.Println("before: ", u)
-
-	// u.Name = "Test2"
-	// u.Email = "test2@example.com"
-	// u.UpdateUser()
-	// fmt.Println("after: ", u)
-
-	// u.DeleteUser()
-	// u, _ = models.GetUser(1)
-	// fmt.Println(u)
-
-	user, _ := models.GetUser(2)
-	user.CreateTodo("first todo")
+	controllers.StartMainServer()
 }
